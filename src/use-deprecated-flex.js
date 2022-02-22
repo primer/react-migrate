0fa41fb23f6b5a9b715c7c4a6ef4198a852b94e3
Siddharth Kshetrapal <siddharthkp@github.com>
2022-02-22 13:12:16 +0100

    Add codemod for Flex

---
+++ src/use-deprecated-flex.js
@@ -0,0 +1,23 @@
+const { SyntaxKind } = require('ts-morph');
+const updateImportDeclaration = require('./utils/change-import-to-deprecated');
+
+const componentImportNames = ['Flex', 'FlexProps'];
+
+const transform = (project) => {
+  const sourceFiles = project.getSourceFiles();
+
+  sourceFiles.forEach((sourceFile) => {
+    try {
+      sourceFile.getDescendantsOfKind(SyntaxKind.ImportDeclaration).forEach((declaration) => {
+        declaration = updateImportDeclaration(declaration, sourceFile, componentImportNames);
+      });
+
+      // save source back to file
+      sourceFile.saveSync();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+module.exports = transform;
