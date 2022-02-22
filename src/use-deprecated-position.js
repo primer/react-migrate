b46eb60b257406807b1a03a5ce5cee420b4759b8
Siddharth Kshetrapal <siddharthkp@github.com>
2022-02-22 13:23:55 +0100

    Add codemod for Position

---
+++ src/use-deprecated-position.js
@@ -0,0 +1,34 @@
+const { SyntaxKind } = require('ts-morph');
+const updateImportDeclaration = require('./utils/change-import-to-deprecated');
+
+const componentImportNames = [
+  'Position',
+  'PositionProps',
+  'Absolute',
+  'AbsoluteProps',
+  'Fixed',
+  'FixedProps',
+  'Relative',
+  'RelativeProps',
+  'Sticky',
+  'StickyProps'
+];
+
+const transform = (project) => {
+  const sourceFiles = project.getSourceFiles();
+
+  sourceFiles.forEach((sourceFile) => {
+    try {
+      sourceFile.getDescendantsOfKind(SyntaxKind.ImportDeclaration).forEach((declaration) => {
+        declaration = updateImportDeclaration(declaration, sourceFile, componentImportNames);
+      });
+
+      // save source back to file
+      sourceFile.saveSync();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+module.exports = transform;
