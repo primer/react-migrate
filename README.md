6a5678d303410e2c07d06f166e37965f3023c9ea
Siddharth Kshetrapal <siddharthkp@github.com>
2022-02-22 16:54:46 +0100

    add available migrations to preset and readme

--- README.md
+++ README.md
@@ -15,7 +15,30 @@ npx primer-react-migrate [src directory] -m [migration name]
 # Example:
 npx primer-react-migrate src/client -p v35
 
-npx primer-react-migrate src/client -m deprecate-buttons
+npx primer-react-migrate src/client -m use-deprecated-button
 ```
 
 &nbsp;
+
+### Available migrations::
+
+```
+use-deprecated-flex.js
+use-deprecated-grid.js
+use-deprecated-position.js
+// todo: use-deprecated-borderbox
+// todo: use-deprecated-dialog
+// todo: use-deprecated-dropdown
+use-deprecated-formgroup.js
+use-deprecated-selectmenu.js
+
+use-deprecated-button
+use-deprecated-actionlist.js
+use-deprecated-actionmenu.js
+use-deprecated-dropdownmenu.js
+
+use-main-button.js
+use-main-actionlist.js
+use-main-actionmenu.js
+use-main-pagelayout.js
+```
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const { resolve } = require('path');
 const args = require('yargs-parser')(process.argv.slice(2));
 const createProject = require('./src/utils/create-project');
@@ -10,18 +11,38 @@ const srcPath = args._[0] || 'src';
 const globPath = resolve(srcPath + '/**/*.{tsx,ts,jsx,js}');
 
 project.addSourceFilesAtPaths(globPath);
-console.log(`Processing ${project.getSourceFiles().length} files at ${globPath} \n`);
+console.log(`${project.getSourceFiles().length} files found at ${globPath} \n`);
 
 const preset = args.preset || args.p;
 const migration = args.migration || args.m;
 
 if (preset === 'v35') {
+  // todo: borderbox
+  require('./src/use-deprecated-flex.js')(project);
+  require('./src/use-deprecated-grid.js')(project);
+  require('./src/use-deprecated-position.js')(project);
+  // todo: dialog
+  // todo: dropdown
+  require('./src/use-deprecated-formgroup.js')(project);
+  require('./src/use-deprecated-selectmenu.js')(project);
+
   require('./src/use-deprecated-button')(project);
-  require('./src/use-deprecated-actionlist')(project);
+  require('./src/use-deprecated-actionlist.js')(project);
+  require('./src/use-deprecated-actionmenu.js')(project);
+  require('./src/use-deprecated-dropdownmenu.js ')(project);
+
+  require('./src/use-main-button.js')(project);
+  require('./src/use-main-actionlist.js')(project);
+  require('./src/use-main-actionmenu.js')(project);
+  require('./src/use-main-pagelayout.js')(project);
 } else {
-  if (migration === 'deprecate-buttons') require('./src/use-deprecated-button')(project);
-  else if (migration === 'use-deprecated-actionlist') require('./src/use-deprecated-actionlist')(project);
-  else console.log('Migration not found! Did you mean `npx primer-react-migrate src -m deprecate-buttons`');
+  const path = './src/' + migration + '.js';
+  if (fs.existsSync(resolve(__dirname, path))) require(path)(project);
+  else {
+    console.log(
+      'Migration not found! Check the list of available migrations on https://github.com/primer/react-migrate'
+    );
+  }
 }
 
 module.export = { project };
